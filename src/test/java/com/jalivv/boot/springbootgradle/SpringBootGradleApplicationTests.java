507b41c101b1e15115a57e51f68277d3d8c13dbc
jalivv <shizuwei99@gmail.com>
2022-07-21 09:24:40 +0800

    first commit

---
+++ src/test/java/com/jalivv/boot/springbootgradle/SpringBootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalivv.boot.springbootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
